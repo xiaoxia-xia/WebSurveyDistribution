a8a92b4830f26a77d9432cb7c3e62366096eb1de
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2022-08-31 15:29:11 -0700

    UPDATE

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react';
+import { createStore, appyMiddleware, applyMiddleware} from 'redux';
+
 
 import App from './components/App';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+<Provider store = {store}> <App /></Provider>,
+document.querySelector('#root')
+);
+
