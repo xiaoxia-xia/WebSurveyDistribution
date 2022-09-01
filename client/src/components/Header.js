9d4730de4390c4b12a27682b59e8299c641c2edd
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2022-09-01 15:22:58 -0700

    update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,45 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 
 class Header extends Component{
+    renderContent(){
+        switch(this.props.auth){
+            case null:
+                return;
+            case false:
+                return <li><a href="/auth/google"> Logined With Google</a></li>
+            default:
+                return <li><a>Logout</a></li>
+        }
+    }
     render(){
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className = "nav-wrapper">
+                    <a className = "left brand-logo">
+                        email
+                    </a>
+                    <ul className = "right">
+                        <li>
+                            {this.renderContent()}
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         )
     }
 }
+function mapStateToProps( {auth} ){
+    return { auth };
+}
 
-export default Header;
+
+// above equal to as 
+// 
+//function mapStateToProps( {state} ){
+//    return { auth: state.auth };
+//}
+
+
+
+export default connect(mapStateToProps)(Header);
