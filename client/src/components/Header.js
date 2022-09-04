70020f04bb688d31a15bfd56e739138b628d0bd8
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2022-09-03 17:07:00 -0700

    for billing action

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,7 +11,10 @@ class Header extends Component{
             case false:
                 return <li><a href="/auth/google"> Logined With Google </a></li>
             default:
-                return <li><a href="/api/logout"> Logout </a></li>
+                return [
+                    <li key = '1'><Payments /></li>,
+                    <li key = '2'><a href="/api/logout"> Logout </a></li>
+                    ];
         }
     }
     render(){
--- client/src/components/Payment.js
+++ client/src/components/Payment.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Payments extends Component{
     render(){
-        debugger;
+
         return(
             <StripeCheckout
+                name="emaily"
+                description='5 dollars for 5 email credit'
                 amount={500}
-                token={token => console.log(token)}
-                stripeKey={process.env.stripeKey}
-            />
+                token={token => this.props.handleToken(token)}
+                stripeKey={process.env.REACT_APP_STRIPE_KEY}
+            >
+                <button className='btn'>
+                    Add Credit 
+                </button>
+
+            </StripeCheckout>
         );
     }
 }
 
-export default Payments;
+export default connect(null, actions)(Payments);
