c79d6bea0b7b6b43beb625ae6e31864423c348f3
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2022-09-04 14:54:55 -0700

    billing completed

--- index.js
+++ index.js
@@ -13,14 +13,12 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 app.use(bodyParser.json());
-
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 3600 * 1000,
         keys: [keys.cookieKey]
     })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,14 +1,12 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')('sk_test_51Le4PDErNSJvU9qbO8eckkuTmW8dK3hyWXDaymyKR9d8p4SxcVT4szsUbpciN5DCSw5nv6j4HXmtfuZVE7c2jNFD00t3bYR61X');
 // I have to use the string here, otherwise if using keys.stripeSecretKey will have 504 error
+const requireLogin = require('../middlewares/requireLogin')
+
+
 
 module.exports = app =>{
-    app.post('/api/stripe', async (req, res) =>{
-        if(!req.user){
-            return res.status(401).send({error: 'you must login'});
-        }
-
-
+    app.post('/api/stripe', requireLogin, async (req, res) => {
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
