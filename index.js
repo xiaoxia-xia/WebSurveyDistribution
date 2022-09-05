bc5f0fe8c51738da6e044acd5a41e0061d439cf1
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2022-09-05 10:55:06 -0700

    Added client side app

--- index.js
+++ index.js
@@ -30,5 +30,20 @@ authRoutes = require('./routes/authRoutes');
 authRoutes(app);
 */
 
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    // epxress will serve up production assests
+    // like our main.js file, or main.css file
+
+    // express will serve up the index.html file, 
+    // if it doesn't recognize the route 
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
