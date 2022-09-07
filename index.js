45e94498b0108c14610ea0a7e49b27c89ca26e0b
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2022-09-06 18:30:06 -0700

    Front-end
    the new model

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 
+require('/models/Survey');
 require('./models/User');
 require('./services/passport');
 
