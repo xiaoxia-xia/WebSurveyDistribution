a91310a0d6d63b71eaed9464606853e56d07223f
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2022-09-10 04:55:00 -0700

    Update surveyRoutes.js

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -3,6 +3,6 @@ const requireCredits = require('../middlewares/requireCredits')
 
 module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) =>{
-
+        req.body
     });
 };
