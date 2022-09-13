6eb8b24b3ffb7880ebfdfe3ca6dfdd925171de9f
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2022-09-12 17:45:55 -0700

    mailer introduced

--- config/pro.js
+++ config/pro.js
@@ -1,9 +1,12 @@
+const { sendGridKey } = require("./dev");
+
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecrete: process.env.GOOGLE_CLIENT_SECRETE,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    stripeSecretKey: process.env.STRIPE_SECRET_KEY
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    sendGridKey:process.env.SEND_GRID_KEY
 
-}
+};
